59c19eded3ac4b1290fe82d2117edddabbb5a30f
Harrisen Sanchez <hmsanchez98@gmail.com>
2019-10-29 21:20:22 -0600

    first commit

---
+++ app/src/main/java/com/example/myaddingmachinepart2/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.myaddingmachinepart2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void addOnClick(View v){
+        TextView tv = findViewById(R.id.textAddend1);
+        String rawAddend1 = tv.getText().toString();
+        double addend1 = Double.parseDouble(rawAddend1);
+
+        TextView tv2 = findViewById(R.id.textAddend1);
+        String rawAddend2 = tv2.getText().toString();
+        double addend2 = Double.parseDouble(rawAddend2);
+
+
+        double result = addend1 + addend2;
+
+        TextView textResult = findViewById(R.id.result);
+        textResult.setText(String.valueOf(result));
+    }
+}
